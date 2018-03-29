13bee330d1b191167a0f131536ec2c955f3e07e6
Daniel Morais <danielmorais@outlook.com.br>
2018-03-29 20:51:23 -0300

    base

--- no.py
+++ no.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+#DANIEL MORAIS - CONTROLE INTELIGENTE - ENG. COMPUTAÇÃO - UFRN 2018.1
+
+class No:
+    def __init__(self, tabela, valor=0, pai=None):
+        self.tabela = tabela    # tabela do jogo
+        self.valor = valor      # valor
+        self.pai = pai          # nó de referência
+        self.filhos = []        # lista de nós filhos
+
+    def addFilho(self, noFilho):
+        self.filhos.append(noFilho)
