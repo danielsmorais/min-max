edf49075140c827177c9ff27dccc8a345e2d49e5
Daniel Morais <danielmorais@outlook.com.br>
2018-03-29 21:28:36 -0300

    base

--- no.py
+++ no.py
@@ -2,12 +2,19 @@
 
 #DANIEL MORAIS - CONTROLE INTELIGENTE - ENG. COMPUTAÇÃO - UFRN 2018.1
 
-class No:
+class No(object):
     def __init__(self, tabela, valor=0, pai=None):
         self.tabela = tabela    # tabela do jogo
         self.valor = valor      # valor
+        self.posicao = None     # verificar uso da posicao
         self.pai = pai          # nó de referência
         self.filhos = []        # lista de nós filhos
 
     def addFilho(self, noFilho):
         self.filhos.append(noFilho)
+    
+
+if __name__ == "__main__":
+    meuno = No(2, 3)
+    meuno.addFilho(No(5, 3, meuno))
+    print(meuno.filhos[0].tabela)
