f0a97d8b35415933fd5fdeb8bf5d87335a36bfd5
Daniel Morais <danielmorais@outlook.com.br>
2018-04-01 12:01:58 -0300

    base

--- no.py
+++ no.py
@@ -3,16 +3,16 @@
 #DANIEL MORAIS - CONTROLE INTELIGENTE - ENG. COMPUTAÇÃO - UFRN 2018.1
 
 class No(object):
-    def __init__(self, tabela, valor=None, pai=None):
-        self.tabela = tabela    # tabela do jogo
-        self.valor = valor      # valor
-        self.posicao = None     # verificar uso da posicao
-        self.pai = pai          # nó de referência
-        self.filhos = []        # lista de nós filhos
+    def __init__(self, tabuleiro, valor=None, pai=None):
+        self.tabuleiro = tabuleiro      # tabela do jogo
+        self.valor = valor              # valor
+        self.posicao = None             # verificar uso da posicao
+        self.pai = pai                  # nó de referência
+        self.filhos = []                # lista de nós filhos
 
     def addFilho(self, noFilho):
         self.filhos.append(noFilho)
-    
+  
 
 if __name__ == "__main__":
     meuno = No(2, 3)
---
+++ tabuleiro.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#DANIEL MORAIS - CONTROLE INTELIGENTE - ENG. COMPUTAÇÃO - UFRN 2018.1
+
+class Tabuleiro(object):
+    
+    tabuleiro = [[None, None, None], [None, None, None], [None, None, None]]
+    winner = ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]) #lista de winners
+
+    def __init__(self, tabuleiro=None):
+        self.tabuleiro = tabuleiro
+
+    def getTabuleiro(self):
+        return self.tabuleiro
+
+    def setTabuleiro(self, tabuleiro):
+        self.tabuleiro = tabuleiro
+
+    def show(self):
+        for element in [self.tabuleiro[i:i + 3] for i in range(0, len(self.tabuleiro), 3)]:
+            print(element)
+
+    def clear(self):
+        self.tabuleiro = [[None, None, None], [None, None, None], [None, None, None]]
+
+        
+if __name__ == "__main__":
+    ta = [[1, 3, 2], [4, 5, 5], [6, 7, 8]]
+    tab = Tabuleiro(ta)
+    tab.show()
+
+
