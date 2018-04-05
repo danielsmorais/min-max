b409d7d2cfd9fbd185a5196dbadcff97f257cd0e
Daniel Morais <danielmorais@outlook.com.br>
2018-04-04 22:34:55 -0300

    base

--- minmax.py
+++ minmax.py
@@ -2,9 +2,24 @@
 
 #DANIEL MORAIS - CONTROLE INTELIGENTE - ENG. COMPUTAÇÃO - UFRN 2018.1
 
+from point import Point
+from no import No
+from tabuleiro import Tabuleiro as Tab
+from arvore import Arvore as Arv
 
-   
-def MinMax( tabuleiro, id):
+class JVelha(object):
         
-    return 1
+    def __init__(self):
+        self.paraVencer = ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                           [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6])
+        self.tab = Tab()
+
+    def minmax(self, id):
+        #TODO chama 
+
+        return 1
+
+    def isVencedor(self, tabuleiro, jogador):
+        pass
+    
         
--- tabuleiro.py
+++ tabuleiro.py
@@ -6,8 +6,6 @@ from point import Point
 
 class Tabuleiro(object):
     
-    winner = ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]) #lista de winners
-
     def __init__(self, tabuleiro=[[None, None, None], [None, None, None], [None, None, None]]):
         self.tabuleiro = tabuleiro
 
